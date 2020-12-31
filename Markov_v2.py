4a2f07adff0501dcdef8053ef7ce4653da09cb21
CruserShadow <cruser_shadow@mail.ru>
2020-12-31 14:37:22 +1000

    array of dicts

--- Markov_v2.py
+++ Markov_v2.py
@@ -2,40 +2,33 @@ import json
 import re
 
 with open("Rules.json", "r") as file:
-    rules = [x for x in json.load(file).items()]
+    rules = [json.load(file)]
 
-count = len(rules)
-check = [i[0] for i in rules]
-change = [i[1] for i in rules]
-
-for i in range(0, count):
-    print(f"{check[i]} -> {change[i]}")
+for key, item in rules[0].items():
+    print(f"{key} -> {item}")
 print()
 
 word = input("Word -> ")
 print(f"> {word} <")
 
-
 #  переменные для цикла
 end = False
 
-for loops in range(100):
+for loops in range(150):
 
-    for i in range(count):
+    for check, change in rules[0].items():
 
-        if check[i] in word:
-            if "." in change[i]:
-                change[i] = change[i].replace(".", "")
-                word = re.sub(check[i], change[i], word, 1)
+        if check in word:
+            if "." in change:
+                change = change.replace(".", "")
+                word = re.sub(check, change, word, 1)
                 end = True
                 print(word)
                 break
 
-            word = re.sub(check[i], change[i], word, 1)
+            word = re.sub(check, change, word, 1)
             print(word)
             break
 
     if end:
         break
-
-
